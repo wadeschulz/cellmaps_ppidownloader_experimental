bcf6c438528904ffdc0a603297e28d80a198c49c
Chris Churas <churas.camera@gmail.com>
2023-06-07 15:27:15 -0700

    updated email author

--- cellmaps_ppidownloader/__init__.py
+++ cellmaps_ppidownloader/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for cellmaps_ppidownloader."""
 
-__author__ = """Gege Qian"""
-__email__ = 'geqian@ucsd.edu'
+__author__ = 'Ideker Lab CM4AI team'
+__email__ = 'tools@cm4ai.org'
 __version__ = '0.1.0a3'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_ppidownloader'
 __description__ = 'A tool to download APMS data for CM4AI pipeline'
