287a9d86ca0bb39f8ccdc5bc13b45a4dabe7b8fa
Chris Churas <churas.camera@gmail.com>
2024-01-22 16:44:11 -0800

    added new integration test that verifies we edge a resulting ppi_edgelist.tsv file

--- tests/test_integration_cellmaps_downloader.py
+++ tests/test_integration_cellmaps_downloader.py
@@ -4,7 +4,9 @@
 """Integration Tests for `cellmaps_ppidownloader` package."""
 
 import os
-
+import csv
+import tempfile
+import shutil
 import unittest
 from cellmaps_ppidownloader import cellmaps_ppidownloadercmd
 
@@ -23,6 +25,56 @@ class TestIntegrationCellmaps_downloader(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_something(self):
+    def get_test_data_dir(self):
+        """
+        get test data dir
+        :return:
+        """
+        return os.path.join(os.path.dirname(__file__), 'data')
+
+    def get_test_provenance(self):
+        """
+        Gets test provenance file path
+        :return:
+        """
+        return os.path.join(self.get_test_data_dir(), 'test_provenance.json')
+
+    def get_baitlist(self):
+        """
+        Gets baitlist file path
+        :return:
+        """
+        return os.path.join(self.get_test_data_dir(), 'baitlist.tsv')
+
+    def get_edgelist(self):
+        """
+        Gets edgelist file path
+        :return:
+        """
+        return os.path.join(self.get_test_data_dir(), 'edgelist.tsv')
+
+    def test_download_with_edgelist_and_baitlist(self):
         """Tests parse arguments"""
-        self.assertEqual(1, 1)
+        temp_dir = tempfile.mkdtemp()
+        try:
+            baitlist = self.get_baitlist()
+            edgelist = self.get_edgelist()
+            run_dir = os.path.join(temp_dir, 'run')
+            provenance = self.get_test_provenance()
+            res = cellmaps_ppidownloadercmd.main(['myprog.py', run_dir,
+                                                  '--edgelist', edgelist,
+                                                  '--baitlist', baitlist,
+                                                  '--provenance', provenance,
+                                                  '--skip_logging'])
+            self.assertEqual(0, res)
+            ppi_edgelist = os.path.join(run_dir, 'ppi_edgelist.tsv')
+            self.assertTrue(os.path.isfile(ppi_edgelist))
+            entries = []
+            with open(ppi_edgelist, 'r', newline='') as f:
+                reader = csv.DictReader(f, delimiter='\t')
+                for row in reader:
+                    entries.append(row)
+            self.assertEqual(2783, len(entries))
+
+        finally:
+            shutil.rmtree(temp_dir)
