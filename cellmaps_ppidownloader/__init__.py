45614fe6d6e3e0dd73f9b3514cf1f31a5e90e353
Chris Churas <churas.camera@gmail.com>
2024-02-01 11:11:38 -0800

    removed alpha from version and set 1st release as today

--- cellmaps_ppidownloader/__init__.py
+++ cellmaps_ppidownloader/__init__.py
@@ -2,9 +2,9 @@
 
 """Top-level package for cellmaps_ppidownloader."""
 
-__author__ = 'Ideker Lab Cell Maps team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a9'
+__version__ = '0.1.0'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_ppidownloader'
 __description__ = 'A tool to download AP-MS data for Cell Maps pipeline'
 __computation_name__ = 'AP-MS Loader'
