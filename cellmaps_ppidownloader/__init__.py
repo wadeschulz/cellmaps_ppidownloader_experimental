58ebde8ce282bb010ba1f7bc0876d778c16a29e9
dzoanka <joanna.lenkiewicz@gmail.com>
2024-08-26 11:18:15 -0700

    Bump version

--- cellmaps_ppidownloader/__init__.py
+++ cellmaps_ppidownloader/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0'
+__version__ = '0.1.1a1'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_ppidownloader'
 __description__ = 'A tool to download AP-MS data for Cell Maps pipeline'
 __computation_name__ = 'AP-MS Loader'
